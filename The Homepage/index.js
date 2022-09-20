b0f44bec69588d1f28952602936c10689faa0d07
gunayakay <101050062+gunayakay@users.noreply.github.com>
2022-09-21 00:21:04 +0300

    Addin functionality for menu

--- The Homepage/index.js
+++ The Homepage/index.js
@@ -1,1 +1,16 @@
-// javascript
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", () => {
+  const visiblity = nav.getAttribute("data-visible");
+  // if the nav is closed, open it
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+  }
+
+  console.log(visiblity);
+  // if the nav is open, close it
+});
